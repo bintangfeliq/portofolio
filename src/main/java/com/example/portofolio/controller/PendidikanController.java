25576bc100c81ce6b24b9b8ebe4a682e959669fd
muhammad bintang feliq <bintangfeliq620@gmail.com>
2026-08-26 20:38:05 +0700

    menambah halama skill dan cotroller login

--- src/main/java/com/example/portofolio/controller/PendidikanController.java
+++ src/main/java/com/example/portofolio/controller/PendidikanController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.portofolio.model.Pendidikan;
 import com.example.portofolio.service.PendidikanService;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
